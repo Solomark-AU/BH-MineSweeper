65dfc6e2b16bbcbcf64f75993d7ecd67eb12f491
Herobrine6265 <herobrine62656@gmail.com>
2024-01-06 23:41:25 +0800

    要提交的变更： 	删除：     execute/generator.cpp 	修改：     include/generator.h 	新文件：   main.cpp
    修改内容：
        基于方便的目的把生成函数的实现丢到了generator.h里

--- include/generator.h
+++ include/generator.h
@@ -6,5 +6,51 @@ public:
     int length, width;
     int map[1005][1005];
 };
-Map generate(int, int, int);
+#include <random>
+#include <ctime>
+Map generate(int length, int width, int num)
+{
+    Map m;
+    m.length = length;
+    m.width = width;
+    bool map[length * width + 5];
+    int area = length * width;
+    for (int i = 1; i <= area; i++)
+        map[i] = 0;
+    if (num < area / 2)
+    {
+        for (int i = area - num + 1; i <= area; i++)
+            map[i] = 1;
+        for (int i = area - num + 1; i <= area; i++)
+        {
+            std::mt19937 random;
+            random.seed(time(0));
+            int pos = random() % (i - 1) + 1;
+            std::swap(map[i], map[pos]);
+        }
+    }
+    else
+    {
+        for (int i = 1; i <= num; i++)
+            map[i] = 1;
+        for (int i = num; i <= area; i++)
+        {
+            std::mt19937 random;
+            random.seed(time(0));
+            int pos = random() % (i - 1) + 1;
+            std::swap(map[i], map[pos]);
+        }
+    }
+    for (int i = 1; i <= length; i++)
+    {
+        for (int j = 1; j <= width; j++)
+        {
+            if (map[(i - 1) * length + j])
+                m.map[i][j] = 1;
+            else
+                m.map[i][j] = 0;
+        }
+    }
+    return m;
+}
 #endif
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "include/generator.h"
+using namespace std;
+int main()
+{
+    return 0;
+}
